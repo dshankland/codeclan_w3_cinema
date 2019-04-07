c21c79361742b9e45f6c2230f39398794e3c122a
Darren Shankland <ds@4pq.co.uk>
2019-04-07 07:48:21 +0100

    add buys_ticket to console.log

--- console.rb
+++ console.rb
@@ -8,7 +8,7 @@ Customer.delete_all()
 Film.delete_all()
 
 customer1 = Customer.new({'name' => 'Ann Campbell', 'funds' => 50})
-customer1.save()3
+customer1.save()
 customer2 = Customer.new({'name' => 'Anne McKendry', 'funds' => 100})
 customer2.save()
 customer3 = Customer.new({'name' => 'David McAllister', 'funds' => 80})
@@ -23,17 +23,22 @@ film2.save()
 film3 = Film.new({'title' => 'Dumbo', 'price' => 10})
 film3.save()
 
-ticket1 = Ticket.new({'customer_id' =>  customer1.id, 'film_id' => film3.id})
-ticket1.save()
-ticket2 = Ticket.new({'customer_id' =>  customer2.id, 'film_id' => film1.id})
-ticket2.save()
-ticket3 = Ticket.new({'customer_id' =>  customer3.id, 'film_id' => film1.id})
-ticket3.save()
-ticket4 = Ticket.new({'customer_id' =>  customer4.id, 'film_id' => film1.id})
-ticket4.save()
-ticket5 = Ticket.new({'customer_id' =>  customer1.id, 'film_id' => film1.id})
-ticket5.save()
-
+# ticket1 = Ticket.new({'customer_id' =>  customer1.id, 'film_id' => film3.id})
+# ticket1.save()
+# ticket2 = Ticket.new({'customer_id' =>  customer2.id, 'film_id' => film1.id})
+# ticket2.save()
+# ticket3 = Ticket.new({'customer_id' =>  customer3.id, 'film_id' => film1.id})
+# ticket3.save()
+# ticket4 = Ticket.new({'customer_id' =>  customer4.id, 'film_id' => film1.id})
+# ticket4.save()
+# ticket5 = Ticket.new({'customer_id' =>  customer1.id, 'film_id' => film1.id})
+# ticket5.save()
+#
+customer1.buys_ticket(film3)
+customer2.buys_ticket(film1)
+customer3.buys_ticket(film1)
+customer4.buys_ticket(film1)
+customer1.buys_ticket(film1)
 
 
 binding.pry
